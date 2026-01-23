f7f0434a8ce027750d2ed29de457c00f45ed1753
iyadshobaki <iyadshobaki82@gmail.com>
2026-01-22 19:39:23 -0500

    feat: implement user controller and routes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const app = express();
+const router = require("./routes/index");
 
 const { PORT = 3001 } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -11,6 +14,8 @@ mongoose
     console.log("Connected to DB");
   })
   .catch(console.error);
+
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
