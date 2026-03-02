e44cf7d69202e96fbcc1ca909bf3bc0342a8e836
iyadshobaki <iyadshobaki82@gmail.com>
2026-03-02 15:51:04 -0500

    feat: add helmet middleware to set security headers for the API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 const cors = require("cors");
 require("dotenv").config();
 const { errors } = require("celebrate");
@@ -10,6 +11,7 @@ const { PORT } = require("./utils/config");
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
